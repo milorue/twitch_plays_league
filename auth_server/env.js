781160c2105d9d50b03675fa381543baeb57c5eb
Milo Rue <milorue@gmail.com>
2020-07-09 13:34:37 -0400

    modifications

--- auth_server/env.js
+++
@@ -1,7 +0,0 @@
-const secrets = {
-    clientId: 'nBjeTkOcxyGUNmofKqlQUbd80OVqbA7yAyEv48SJ',
-    clientSecret: 'EB0VjSzrai3T0YXVnABFefoGqSoAEHylrYiPcH4I',
-    redirectURI: 'http://localhost:8080/auth'
-}
-
-module.exports = secrets
